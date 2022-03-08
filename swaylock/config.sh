9482feaca8ba7f52568626f04b5535a32b6748f4
Paulobergine <pguerreftb@gmail.com>
2022-03-08 14:22:12 -0500

    Pastel style for sway

--- swaylock/config.sh
+++ swaylock/config.sh
@@ -4,6 +4,7 @@ swayidle \
     timeout 10 'swaymsg "output * dpms off"' \
     resume 'swaymsg "output * dpms on"' &
 # Locks the screen immediately
-swaylock --screenshots --clock -l --bs-hl-color ff0000 --indicator-idle-visible --inside-color 20466a --inside-clear-color 1f70bd --inside-ver-color 1f70bd --inside-wrong-color ff0000 --key-hl-color ffffff --line-color 1f70bd --line-clear-color 20466a --line-ver-color 20466a --line-wrong-color ff0000 --ring-color 20466a --ring-clear-color 1f70bd --ring-ver-color 1f70bd --ring-wrong-color ff0000 --separator-color 1f70bd --text-color ffffff --text-clear-color ffffff --text-ver-color ffffff --text-wrong-color 000000 --effect-blur 4x2
+swaylock --screenshots --clock --indicator-idle-visible --inside-color 292b2e --inside-clear-color 292b2e --inside-ver-color 292b2e --inside-wrong-color 292b2e --key-hl-color 292b2e --line-color 292b2e --line-clear-color 292b2e --line-ver-color 292b2e --line-wrong-color 292b2e --ring-color 292b2e --ring-clear-color 292b2e --ring-ver-color 292b2e --ring-wrong-color 292b2e --separator-color 292b2e --text-color ffffff --text-clear-color ffffff --text-ver-color ffffff --text-wrong-color ff6961 --effect-blur 4x2 --text-clear Cleared --text-caps-lock Maj --text-ver Checking --text-wrong Cheh --bs-hl-color 292b2e
+#swaylock
 # Kills last background task so idle timer doesn't keep running
 kill %%
